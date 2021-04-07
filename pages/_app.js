3072cad5bc1fd536c2c48d007b1d90b80ea79594
Praveen Kumar <pkspyder007@gmail.com>
2021-04-07 18:06:03 +0530

    Initial Sections

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
-import '../styles/globals.css'
+import "pure-react-carousel/dist/react-carousel.es.css";
+import "../styles/globals.css";
+import "../styles/main.css";
+import Head from "next/head";
+import Layout from "@components/Layout";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Head>
+        <title>Home | Praveen Kumar</title>
+        <meta
+          name="Description"
+          content="Force is the biggest coding club of IIIT Una lead by a group of phenomenal devs helping others to learn and grow in field of computer science."
+        />
+      </Head>
+
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ sections/Hero.js
@@ -0,0 +1,35 @@
+export default function Hero() {
+  return (
+    <section className="text-gray-700 body-font">
+      <div className="container flex flex-col items-center px-5 pb-16 pt-4 mx-auto md:flex-row">
+        <div className="flex flex-col items-center w-full pt-0 mb-16 text-left lg:flex-grow md:w-1/2 xl:mr-20 md:pr-24 md:items-start md:text-left md:mb-0 lg:text-center">
+          <h2 className="mb-1 text-sm font-medium tracking-widest text-black title-font"></h2>
+          <h1 className="mb-8 text-2xl font-bold tracking-wide text-center text-black lg:text-left lg:text-5xl title-font">
+            <span className="leading-12 ">
+              Creating Digital Solutions to solve modern problems.
+            </span>
+          </h1>
+          <p className="mb-8 text-base leading-relaxed text-center text-gray-700 lg:text-left lg:text-1xl">
+            I am Praveen, a software engineer and <u>web developer</u>. Let me
+            help you in bringing your ideas to life.
+          </p>
+          <div className="flex justify-center">
+            <button className="flex items-center px-6 py-2 mt-auto font-semibold text-white transition duration-500 ease-in-out transform bg-black rounded-lg hover:bg-gray-200 hover:text-black focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2">
+              Let's Talk
+            </button>
+            <button className="flex items-center px-6 py-2 ml-4 mt-auto font-semibold text-black transition duration-500 ease-in-out transform bg-gray-200 rounded-lg hover:bg-gray-800 hover:to-black hover:text-white focus:shadow-outline focus:outline-none focus:ring-2 ring-offset-current ring-offset-2">
+              Explore More
+            </button>
+          </div>
+        </div>
+        <div className="w-5/6 lg:max-w-lg lg:w-full md:w-1/2">
+          <img
+            className="object-cover object-center rounded-lg"
+            alt="hero"
+            src="/img/hero.jpeg"
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
